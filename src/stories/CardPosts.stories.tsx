b390189d6fc3a361bb9caeea3be736345b14085d
CristianGomezO <cristiangomezosorio17@gmail.com>
2022-02-02 19:47:32 -0500

    storybook added

---
+++ src/stories/CardPosts.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { CardPosts } from "../components/molecules/cardPosts";
+
+import '../styles/fragments/_cardPosts.scss'
+
+export default {
+  title: "Poster/CardPost",
+  component: CardPosts,
+} as ComponentMeta<typeof CardPosts>;
+
+const Template: ComponentStory<typeof CardPosts> = (args) => (
+  <CardPosts {...args} />
+);
+
+export const UserCard = Template.bind({});
+UserCard.args = {
+  ...UserCard.args,
+};
