03d1a896db4d1c3acadbfd642e91f1db5612b609
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-03-05 12:45:58 -0600

    feature: Add large button option

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -4,13 +4,17 @@ export interface ButtonProps {
   children?: ReactNode;
   className?: string;
   type?: string;
+  size?: string;
 }
 const Button: FunctionComponent<ButtonProps> = ({
   children,
   className,
   type,
+  size,
 }) => {
-  return <button className={`btn ${type} ${className}`}>{children}</button>;
+  return (
+    <button className={`btn ${type} ${className} ${size}`}>{children}</button>
+  );
 };
 
 export default Button;
