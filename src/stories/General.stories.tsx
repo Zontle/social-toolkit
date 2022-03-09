a5c5a6a467313c52d3259be2195e0300ccdc9253
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-03-09 13:39:14 -0600

    storybook: Add general section

---
+++ src/stories/General.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Button from '../components/atoms/button';
+
+import '../styles/main.scss';
+
+export default {
+  title: 'General/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  ...Primary.args,
+  type: 'primary',
+  children: 'Hola',
+};
