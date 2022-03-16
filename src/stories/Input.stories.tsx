b8bddb66c9f738747217b3e4b2289460373ffc9a
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-03-15 21:05:04 -0600

    storybook: Add input component

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Input from '../components/atoms/input';
+
+import '../styles/main.scss';
+
+export default {
+  title: 'General/Input',
+  component: Input,
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const RegularInput = Template.bind({});
+RegularInput.args = {
+  ...RegularInput.args,
+  placeholder: 'This is a regular input!',
+};
+
+export const RegularTextArea = Template.bind({});
+RegularTextArea.args = {
+  ...RegularTextArea.args,
+  type: 'textarea',
+  placeholder: 'This is a regular textarea!',
+};
