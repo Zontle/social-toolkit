59c51aa3a36108f2f3d3fb3ce0c77598664ff280
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-03-18 14:29:56 -0600

    feature: Add trending component

---
+++ src/components/molecules/trending.tsx
@@ -0,0 +1,38 @@
+import { FunctionComponent } from 'react';
+import { Card } from './card';
+
+interface TrendingProps {
+  className?: string;
+  username?: string;
+  items?: Array<string>;
+  title?: string;
+  layout?: string; //vertical horizontal
+}
+
+export const Trending: FunctionComponent<TrendingProps> = ({
+  className,
+  username,
+  title = 'Trending for',
+  items = [],
+  layout = 'horizontal',
+}) => {
+  const _renderList = () => {
+    return items.map((trend, index) => (
+      <div className="trend-item">
+        {index + 1} {trend}
+      </div>
+    ));
+  };
+  return (
+    <div className={`trending ${className}`}>
+      <h1>Trending</h1>
+      <Card>
+        <div className="meta-info">
+          <h3>{title}</h3>
+          <p>@{username}</p>
+        </div>
+        <div className={`list-container ${layout}`}>{_renderList()}</div>
+      </Card>
+    </div>
+  );
+};
