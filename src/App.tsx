a8a665df718ce08f4881e2643de095f54fa38491
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-03-18 15:04:18 -0600

    fix: Change app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import { Posts } from "./components/organisms/posts";
+import { Posts } from './components/organisms/posts';
+
+import { CardPosts } from './components/molecules/cardPosts';
+import { PostsSection } from './components/molecules/postsSection';
 
 function App() {
-  return <Posts />;
+  const trending = ['#RealTime', '#Blog', '#Collection', '#Popular'];
+  return (
+    <Posts layout="timeline" trending={trending}>
+      <PostsSection type="row">
+        <CardPosts
+          active
+          user="Cristian"
+          username="CristianGO"
+          attachments={[]}
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quo voluptate odio aliquid maxime amet commodi tenetur possimus et perferendis."
+          publishTime="now"
+        />
+        <CardPosts
+          user="Cristian"
+          username="CristianGO"
+          attachments={[]}
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quo voluptate odio aliquid maxime amet commodi tenetur possimus et perferendis."
+          publishTime="now"
+        />
+        <CardPosts
+          user="Cristian"
+          username="CristianGO"
+          attachments={[]}
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quo voluptate odio aliquid maxime amet commodi tenetur possimus et perferendis."
+          publishTime="now"
+        />
+      </PostsSection>
+    </Posts>
+  );
 }
 
 export default App;
