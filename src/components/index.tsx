c8eca2b13e164393e07a579261dc8f6eb9b64a1e
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-03-22 14:25:19 -0600

    feature:Add global index

---
+++ src/components/index.tsx
@@ -0,0 +1,30 @@
+export type { ButtonProps } from './atoms/button';
+export { default as Button } from './atoms/button';
+
+export type { InputProps } from './atoms/input';
+export { default as Input } from './atoms/input';
+
+export type { CardProps } from './molecules/card';
+export { Card } from './molecules/card';
+
+export type {
+  Attachment,
+  CardPostsProps as PostProps
+} from './molecules/cardPosts';
+
+export { CardPosts as Post } from './molecules/cardPosts';
+
+export type { MenuProps } from './molecules/menu';
+export { default as Menu } from './molecules/menu';
+
+export type { PostsSectionProps as PostContainerProps } from './molecules/postsSection';
+export { PostsSection as PostContainer } from './molecules/postsSection';
+
+export type { TrendingProps } from './molecules/trending';
+export { default as Trending } from './molecules/trending';
+
+export type { TextPostProps as PostWriterProps } from './molecules/TextPost';
+export { default as PostWriter } from './molecules/TextPost';
+
+export type { PostsProps as LayoutProps } from './organisms/posts';
+export { Posts as Layout } from './organisms/posts';
