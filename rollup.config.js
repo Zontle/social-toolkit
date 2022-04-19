1dcc374ab84e592a256932250f8579fb6691ae29
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-04-19 16:53:40 -0500

    Update rollup setup

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ import path from 'path';
 const packageJson = require('./package.json');
 
 export default {
-  input: 'src/components/index.tsx',
+  input: 'src/index.tsx',
   output: [
     {
       file: packageJson.main,
