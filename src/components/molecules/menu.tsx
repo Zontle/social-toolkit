edbb778789c8c04afc762020e8e81b6acd02945e
Alfredo Medrano Sánchez <alfredomedranosanchez@gmail.com>
2022-05-20 11:00:12 -0500

    ui: Add header with drawer

--- src/components/molecules/menu.tsx
+++ src/components/molecules/menu.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
-import { FunctionComponent } from 'react';
 import { SidebarElement, MenuOnChange } from '../../types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 //Hooks
 import { useMobile } from '../../hooks/responsive.hook';
 export interface MenuProps {
@@ -24,8 +25,57 @@ const Menu: FunctionComponent<MenuProps> = ({
   onChange = () => ({})
 }) => {
   const isMobile = useMobile();
+  const [visible, setVisible] = useState<boolean>(false);
   if (isMobile) {
-    return <div>{children}</div>;
+    return (
+      <div className="container-mobile">
+        <header>
+          <FontAwesomeIcon
+            onClick={() => setVisible(true)}
+            icon={faBars}
+            className="menu-trigger"
+          />
+        </header>
+        <main>{children}</main>
+        <div className={`sidebar-drawer ${visible ? 'visible' : ''}`}>
+          <div className="cover" onClick={() => setVisible(false)}></div>
+          <div className="container-menu slideInLeft">
+            <div className="sidebar-navigation">
+              <div className="top-sidebar">
+                <div className="detail-top-icon">
+                  {topIcons?.length && (
+                    <div className="icon">{topIcons.map(x => x.icon)}</div>
+                  )}
+                  {wake ? <div className="wake">{wake.icon}</div> : null}
+                </div>
+
+                <div className="center-sidebar">
+                  <ul>
+                    {centerIcons?.map((x, i) => (
+                      <li key={i} onClick={() => onChange(x.slug)}>
+                        <div
+                          className={`center-sidebar-element ${
+                            x.className || ''
+                          } ${active === x.slug && 'active'}`}
+                        >
+                          {x.icon}
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <div className="bottom-sidebar">
+                {wake ? <div className="wake">{wake.icon}</div> : null}
+                {bottomIcons?.length && (
+                  <div className="icon">{bottomIcons.map(x => x.icon)}</div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
   return (
     <div className="container-menu">
