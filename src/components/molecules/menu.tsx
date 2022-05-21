4f6166343b321646fd6a38a673a3150b94223267
Alfredo Medrano Sánchez <alfredomedranosanchez@gmail.com>
2022-05-21 11:51:17 -0500

    feature: Add settings menu handler

--- src/components/molecules/menu.tsx
+++ src/components/molecules/menu.tsx
@@ -102,7 +102,12 @@ const Menu: FunctionComponent<MenuProps> = ({
         </div>
         <div className="bottom-sidebar">
           {bottomIcons?.length && (
-            <div className="icon">{bottomIcons.map(x => x.icon)}</div>
+            <div
+              className={`icon  ${active === 'settings' ? 'active' : ''}`}
+              onClick={() => onChange('settings')}
+            >
+              {bottomIcons.map(x => x.icon)}
+            </div>
           )}
         </div>
       </div>
