aaba90d19632226bef283c90537db0f998219d01
Alfredo Medrano Sánchez <alfredomedranosanchez@gmail.com>
2022-05-21 12:31:01 -0500

    feature: Add classname and noIcons props

--- src/components/molecules/postsSection.tsx
+++ src/components/molecules/postsSection.tsx
@@ -7,26 +7,30 @@ export interface PostsSectionProps {
   title?: string;
   type?: 'column' | 'row';
   children?: React.ReactNode;
+  noIcons?: boolean;
+  className?: string;
 }
 
 export const PostsSection: FunctionComponent<PostsSectionProps> = ({
   title = '',
   children,
-  type = 'column'
+  type = 'column',
+  noIcons = false,
+  className = ''
 }) => {
   if (type === 'row') {
     return (
-      <div className={`post-section ${type}`}>
+      <div className={`post-section ${className} ${type}`}>
         <div className="container">{children}</div>
       </div>
     );
   }
   return (
-    <div className={`post-section ${type}`}>
+    <div className={`post-section ${className} ${type}`}>
       <div className="container">
         <div className="top-post">
           {title && <h1>{title}</h1>}
-          <IconMenuPostSection size="2x" />
+          {!noIcons && <IconMenuPostSection size="2x" />}
         </div>
         {children}
       </div>
