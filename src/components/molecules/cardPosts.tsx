e08245cc99031b495cca9e72ee35066b614d9744
Alfredo Medrano Sánchez <alfredomedranosanchez@gmail.com>
2022-05-25 21:36:35 -0500

    feature: Add carousel support

--- src/components/molecules/cardPosts.tsx
+++ src/components/molecules/cardPosts.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import dayjs, {type Dayjs} from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime'
+import dayjs, { Dayjs } from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import { FunctionComponent } from 'react';
 import { CardMessageIcon } from '../../assets/icons/cardMessage';
 import { HeartIcon } from '../../assets/icons/heart';
 import { RetweetIcon } from '../../assets/icons/retweet';
 import { IconMenu } from '../atoms/icons';
 import { Card } from './card';
+import { Carousel } from 'react-responsive-carousel';
+
 import { parseTimeFromNow } from '../../utils/time';
 export interface Attachment {
   url?: string;
@@ -35,11 +37,22 @@ export const CardPosts: FunctionComponent<CardPostsProps> = ({
   noActions = false
 }) => {
   const _renderAttachments = () => {
+    if (attachments.length > 1) {
+      return (
+        <Carousel showThumbs={false}>
+          {attachments?.map((attachment, i) => (
+            <div className="post-files" key={i}>
+              <img alt="img" src={attachment?.url} />
+            </div>
+          ))}
+        </Carousel>
+      );
+    }
     return attachments?.map((attachment, i) => {
       if (attachment?.type === 'image') {
         //TODO Add carousel view
         if (i >= 1) {
-          return <div key={i}></div>
+          return <div key={i}></div>;
         }
         return (
           <div className="post-files" key={i}>
@@ -55,19 +68,19 @@ export const CardPosts: FunctionComponent<CardPostsProps> = ({
       //Limit number of characters
       if (content.length > CHAR_CONTENT_LIMIT) {
         if (attachments.length > 0) {
-          return content.substring(0, CHAR_CONTENT_LIMIT - 150) + '...'
+          return content.substring(0, CHAR_CONTENT_LIMIT - 150) + '...';
         }
-        return content.substring(0, CHAR_CONTENT_LIMIT) + '...'
+        return content.substring(0, CHAR_CONTENT_LIMIT) + '...';
       }
     }
-    return content
-  }
+    return content;
+  };
   return (
     <Card
       className={`card-posts ${attachments?.length > 0 ? 'media' : ''}`}
       active={active}
     >
-      <div className='post-content'>
+      <div className="post-content">
         <div className="username-publish-time">
           <div>{user}</div>
           <div className="publish-time">{parseTimeFromNow(publishTime)}</div>
@@ -78,35 +91,35 @@ export const CardPosts: FunctionComponent<CardPostsProps> = ({
           <div>{_parseContent()}</div>
         </div>
       </div>
-      {!noActions &&  <div className="icons">
-        <div className="left-bottom-card">
-          <HeartIcon
-            width="17.5"
-            height="16.279"
-            fill="#676666"
-            stroke="#676666"
-            strokeWidth="0.5"
-          />
-          <RetweetIcon
-            width="18.776"
-            height="19.828"
-            fill="none"
-            stroke="#676666"
-          />
-          <CardMessageIcon
-            width="19"
-            height="19"
-            fill="none"
-            stroke="#676666"
-            strokeWidth="2"
-          />
+      {!noActions && (
+        <div className="icons">
+          <div className="left-bottom-card">
+            <HeartIcon
+              width="17.5"
+              height="16.279"
+              fill="#676666"
+              stroke="#676666"
+              strokeWidth="0.5"
+            />
+            <RetweetIcon
+              width="18.776"
+              height="19.828"
+              fill="none"
+              stroke="#676666"
+            />
+            <CardMessageIcon
+              width="19"
+              height="19"
+              fill="none"
+              stroke="#676666"
+              strokeWidth="2"
+            />
+          </div>
+          <div className="icon-menu">
+            <IconMenu />
+          </div>
         </div>
-        <div className="icon-menu">
-          <IconMenu />
-        </div>
-
-      </div>}
-
+      )}
     </Card>
   );
 };
